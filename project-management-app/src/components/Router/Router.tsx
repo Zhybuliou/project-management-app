677b300960f92d56fac39850331a89f0cf6021dd
Katherina Kaliaha <mellinda077@mail.ru>
2022-11-10 16:52:07 +0300

    feat: add pages signIn, signUp, temporary forms with api requests connection #11, #12, #41

--- project-management-app/src/components/Router/Router.tsx
+++ project-management-app/src/components/Router/Router.tsx
@@ -1,6 +1,8 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { PrivateAuth } from '../../hoc/PrivateAuth';
 import { useAppSelector } from '../../hook';
+import { SignIn } from '../../pages/signIn/SignIn';
+import { SignUp } from '../../pages/signUp/SignUp';
 
 export const Router = () => {
   const auth = useAppSelector((state) => state.auth.auth);
@@ -9,8 +11,8 @@ export const Router = () => {
     <Routes>
       <Route path='/' element={<h1>Welcome page</h1>}></Route>
       <Route path='/main' element={<PrivateAuth><h1>Main page</h1></PrivateAuth>}></Route>
-      <Route path='/login' element={!auth ? <h1>Sign in page</h1> : <Navigate to={'/'}/>}></Route>
-      <Route path='/register' element={!auth ? <h1>Sign up page</h1> : <Navigate to={'/'}/>}></Route>
+      <Route path='/login' element={!auth ? <SignIn /> : <Navigate to={'/main'}/>}></Route>
+      <Route path='/register' element={!auth ? <SignUp /> : <Navigate to={'/main'}/>}></Route>
       <Route path='/profile' element={<PrivateAuth><h1>Profile page</h1></PrivateAuth>}></Route>
       <Route path='/board/:id' element={<PrivateAuth><h1>Board page</h1></PrivateAuth>}></Route>
       <Route path='*' element={<h1>Error page</h1>}></Route>
