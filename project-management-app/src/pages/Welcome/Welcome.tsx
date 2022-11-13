846bc93169c7b62c9695f3d65afa03b1d984eddc
EVGEN <k.e.l@tut.by>
2022-11-13 14:10:40 +0300

    feat: render footer

--- project-management-app/src/pages/Welcome/Welcome.tsx
+++ project-management-app/src/pages/Welcome/Welcome.tsx
@@ -1,3 +1,9 @@
+import { Container } from '@mui/material';
+
 export const Welcome = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <Container className='main' maxWidth='xl' component='main' sx={{ flex: '1 1 auto' }}>
+      <h1>Welcome</h1>
+    </Container>
+  );
 };
