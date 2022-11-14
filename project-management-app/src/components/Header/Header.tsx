32de330a0888b47a8d10b16f7fb99735e87cebdc
Katherina Kaliaha <mellinda077@mail.ru>
2022-11-14 12:11:08 +0300

    fix: delete superflous code

--- project-management-app/src/components/Header/Header.tsx
+++ project-management-app/src/components/Header/Header.tsx
@@ -4,23 +4,6 @@ import { NavLink } from 'react-router-dom';
 import mainLogo from '../../assets/MainLogo.png';
 import { Menu } from './components/Menu/Menu';
 
-// export const Header = () => {
-//   return (
-//     <Container maxWidth='xl'>
-//       <AppBar position='static' sx={{ boxShadow: 'none' }}>
-//         <Toolbar component='nav' sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-//           <NavLink to='/'>
-//             <IconButton size='small'>
-//               <img src={mainLogo} style={{ width: '70px' }} alt='Project Logo' />
-//             </IconButton>
-//           </NavLink>
-//           <Menu />
-//         </Toolbar>
-//       </AppBar>
-//     </Container>
-//   );
-// };
-
 interface Props {
   children: React.ReactElement;
 }
